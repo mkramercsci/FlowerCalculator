91b3ebce37b4f88237aa89c796997c5b25318385
Michaela <mkramer.csci@gmail.com>
2022-07-06 13:54:00 -0500

    added a matrix that stores the hybrid level of a flower

--- app/src/main/java/edu/niu/cs/z1860050/flowercalculator/MainActivity.java
+++ app/src/main/java/edu/niu/cs/z1860050/flowercalculator/MainActivity.java
@@ -3,7 +3,10 @@ package edu.niu.cs.z1860050.flowercalculator;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity
 {
   TextView mainTV;
   Button button, redBtn;
+  Spinner speciesSpin;
 
   private static final Integer NUM_COLORS = 9;
   private static final Integer NUM_SPECIES = 8;
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity
   ColorData colorsData = new ColorData();
   SpeciesData speciesData = new SpeciesData();
 
+  //holds all flowers that exist in the game
   ArrayList<Flower> flowers = new ArrayList<>();
 
   @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
@@ -39,11 +44,46 @@ public class MainActivity extends AppCompatActivity
     redBtn = findViewById(R.id.redButton);
     redBtn.setText("Red");
 
-    //insert the values into their containers
+    //connect to the on screen spinner
+    speciesSpin = findViewById(R.id.speciesSpinner);
+
+    //attach the adapter to the spinner
+    ArrayAdapter<String> javaAdapter = new ArrayAdapter<>(this,
+        R.layout.spinner_view, SpeciesData.speciesValues);
+
+    //connect the adapter and spinner
+    speciesSpin.setAdapter(javaAdapter);
+
+    //set up the listener
+    speciesSpin.setOnItemSelectedListener(spinnerListener);
+
+    //make all the existing flower objects
     buildAllFlowers();
 
+    //test that all the flowers were created appropriately
     mainTV.setText("total = " + flowers.size());
+
   }
+
+  //build the listener which detects the user's spinner selection
+  private AdapterView.OnItemSelectedListener spinnerListener = new AdapterView.OnItemSelectedListener()
+  {
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
+    {
+      String selection;
+
+      //get the selection from the spinner
+      selection = adapterView.getItemAtPosition(position).toString();
+
+      //selection contains a string of the species selected by the user. pass it somewhere
+      //doSomething(selection);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView)
+    { }
+  };//end OnItemSelectedListener
 
   //method to build all the valid flowers in the game
   public void buildAllFlowers()
@@ -61,6 +101,7 @@ public class MainActivity extends AppCompatActivity
     boolean isWindflowers[] = {true, true, false, true, true, true, true, false, false};
     boolean isPansies[] = {true, true, true, true, false, true, true, false, false};
 
+    //does this species/color combo exist?
     boolean exists[][] = new boolean [NUM_SPECIES][NUM_COLORS];
     exists[0] = isRoses;
     exists[1] = isTulips;
@@ -71,7 +112,20 @@ public class MainActivity extends AppCompatActivity
     exists[6] = isWindflowers;
     exists[7] = isPansies;
 
-    //traverse the species/color matrix in order and build only the flowers that exist in the game
+    // hybrid matrix for roses
+    Integer hybridRoses[][] = new Integer[][] {
+        new Integer[] {0},    //red
+        new Integer[] {0, 1}, //white
+        new Integer[] {0},    //yellow
+        new Integer[] {1, 2}, //orange
+        new Integer[] {3},    //pink
+        new Integer[] {3},    //blue
+        new Integer[] {1, 2}, //purple
+        new Integer[] {3},    //black
+        new Integer[] {null}  //green
+    };
+
+    //traverse the species/color matrix and build only the flowers that exist in the game
     for (int currSpecies = 0; currSpecies < NUM_SPECIES; currSpecies++)
     {
       for (int currColor = 0; currColor < NUM_COLORS; currColor++)
@@ -84,8 +138,11 @@ public class MainActivity extends AppCompatActivity
         }
       }//end inner for
     }//end outer for
+    
   }//end buildAllFlowers
 
+  //test function to prove all the flowers built successfully
+  //currently not connected to any button
   public void changeText(View view)
   {
     String showThis = "";
@@ -101,5 +158,28 @@ public class MainActivity extends AppCompatActivity
 
     //display the flowers
     mainTV.setText(showThis);
+  }//end changeText
+
+  //method to update the screen when a color button is clicked
+  public void onClickColor(View view)
+  {
+    //we want to update two scrollviews:
+      //list of ways to obtain selected flower
+      //list of recipes involving selected flower
+
+    //find out which flower we want
+      //make a determineSelected method?
+
+    //check the list of recipes where result = currentFlower
+      //use a boolean flag to check seedbag colors, hybrids may have multiple recipes
+
+    //check the list of recipes where parent A or B = currentFlower
+      //indicate hybrid level with a number 0-3
+      // 3 = seedbag
+      // 2 = blue hybrid
+      // 1 = yellow hybrid
+      // 0 = final hybrid
+
   }
-}
+
+}// end MainActivity
--- app/src/main/java/edu/niu/cs/z1860050/flowercalculator/SpeciesData.java
+++ app/src/main/java/edu/niu/cs/z1860050/flowercalculator/SpeciesData.java
@@ -3,7 +3,7 @@ package edu.niu.cs.z1860050.flowercalculator;
 public class SpeciesData
 {
   static public String speciesValues[] =
-      {"roses", "tulips", "hyacinths", "lilies", "cosmos", "mums", "windflowers", "pansies"};
+      {"Roses", "Tulips", "Hyacinths", "Lilies", "Cosmos", "Mums", "Windflowers", "Pansies"};
 
   //retrieve values from the array
   public String get(int i)
